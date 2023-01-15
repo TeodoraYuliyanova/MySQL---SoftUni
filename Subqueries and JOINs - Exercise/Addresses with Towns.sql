ed8da2abde3e336c576f4f932708432f64e900a5
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-01-15 22:18:18 +0000

    Add files via upload

---
+++ Subqueries and JOINs - Exercise/Addresses with Towns.sql
@@ -0,0 +1,6 @@
+SELECT e.`first_name`,e.`last_name`,t.`name` AS `town`,a.`address_text`
+FROM `employees` e JOIN  `addresses` a
+ON e.`address_id` = a.`address_id`
+JOIN `towns` t ON a.`town_id` = t.`town_id`
+ORDER BY e.`first_name`,e.`last_name`
+LIMIT 5;
