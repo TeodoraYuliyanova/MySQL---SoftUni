b4c3b31decba42c06dc85198d50a199c8b1f40f4
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-01-30 23:23:47 +0000

    Add files via upload

---
+++ Subqueries and JOINs - Exercise/Count Mountain Ranges.sql
@@ -0,0 +1,7 @@
+SELECT c.`country_code`,COUNT(m.`mountain_range`) AS `mountain_range`
+FROM `countries` c 
+JOIN `mountains_countries` mc ON c.`country_code` = mc.`country_code`
+JOIN `mountains` m ON mc.`mountain_id` = m.`id`
+WHERE c.`country_code` IN ("BG","RU","US")
+GROUP BY c.`country_code`
+ORDER BY `mountain_range` DESC;
